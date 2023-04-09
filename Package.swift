2c92efd436415250ea6021fe552bfb41e84c4f7d
Adebayo Jagunmolu <hardebahyho@gmail.com>
2023-04-09 12:45:00 +0100

    Changed target name

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "KMMLib",
-            targets: ["KMMLib"])
+            name: "sdk",
+            targets: ["sdk"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "KMMLib",
+            name: "sdk",
             url: "https://github.com/bayo-code/KMMTestLib/raw/main/sdk.xcframework.zip",
             checksum: "42aadc92c512cbdf2c51ab850b51e02da03854a02c558b93c5ab33a73bfee461"
         ),
